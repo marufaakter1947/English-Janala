6ba2932c452c7dc496b8b27b5eb1c8720a1a8aff
Marufa Akter <marufaakter1947@gmail.com>
2025-09-04 16:30:59 +0600

    sound vocabulary added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
 return(htmlElements.join(" "));
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status)=>{
 if(status==true){
     document.getElementById("spinner").classList.remove("hidden");
@@ -95,7 +101,7 @@ for(let word of words){
             <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাত্তয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাত্তয়া যায়নি"}"</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
 
         </div>
@@ -124,3 +130,18 @@ levelContainer.append(btnDiv);
 };
 
 loadLessons();
+
+document.getElementById("btn-search").addEventListener("click",()=>{
+    removeActive();
+    const input = document.getElementById("input-search");
+    const searchValue= input.value.trim().toLowerCase() ;
+    console.log(searchValue);
+    fetch(`https://openapi.programming-hero.com/api/words/all`)
+    .then(res => res.json())
+    .then(data =>{
+        const allWords = data.data;
+        console.log(allWords);
+        const filterWords=allWords.filter(word=>word.word.toLowerCase().includes(searchValue));
+        displayLevelWord(filterWords)
+    });
+})
