b3296ba0ff58900d7c24b694afd933b59b43ff6a
Marufa Akter <marufaakter1947@gmail.com>
2025-09-04 12:08:50 +0600

    Empty lessons error handle added

--- index.js
+++ index.js
@@ -16,14 +16,23 @@ const displayLevelWord =(words)=>{
 const wordContainer =document.getElementById("word-container");
 wordContainer.innerHTML="";
 
+if(words.length == 0){
+wordContainer.innerHTML=` <div class="text-center col-span-full space-y-6">
+ <img class="mx-auto" src="./assets/alert-error.png" alt="error image">
+        <p class="text-xl font-semibold text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl" >নেক্সট Lesson এ যান</h2>
+       </div>`;
+    return;
+}
+
 for(let word of words){
     const card =document.createElement("div");
     card.innerHTML=`
     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাত্তয়া যায়নি"}</h2>
             <p class="font-semibold">Meaning /Pronunciation</p>
 
-            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাত্তয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাত্তয়া যায়নি"}"</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
